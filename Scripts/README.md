5913b7e0e242433bf31e769564796e63eafa47ef
neel1998 <neellovepainting@gmail.com>
2020-08-10 11:37:24 +0530

    readme and scripts changed

--- Scripts/README.md
+++ Scripts/README.md
@@ -2,10 +2,9 @@
 
 This folder contains script which can be used to parse the VIBE skeleton data of skeletics-152 and skeleton-mimetics
 
-There are two main scripts:
+There is one main script:
 
-1. <b>create_vacnn_data.py</b> (Parses VIBE data into VaCNN format)
-2. <b>create_gcn_data.py</b> (Parses VIBE data into GCN format)
+1. <b>parse_vibe_data.py</b> (Parses VIBE data into VaCNN and GCN format)
 
 
 - VaCNN data is stored in <b>.h5</b> format
@@ -23,14 +22,17 @@ The data is stored in <b>N x C x T x V x M </b>, where
 - M = Number of Persons (M = 2 by default)
 
 
-### How to run scripts
+### How to run script
 
-```python3 create_vacnn_data.py INPUT_PATH OUTPUT_PATH```
-
-
-```python3 create_gcn_data.py INPUT_PATH OUTPUT_PATH```
+```python3 parse_vibe_data.py INPUT_PATH OUTPUT_PATH```
 
  Where
 
  - INPUT_PATH =  path to the directory containing VIBE .json files
  - OUTPUT_PATH =  path where parsed data needs to be saved.
+
+After the script is finished a <b>vacnn_data.h5</b> file will be created with VaCNN data and labels.
+
+Also two more files will be created, <b> gcn_train_data.npy </b> and <b> gcn_train_label.npy </b> which will contain data and label respectively.
+
+<i><b>(NOTE: To parse different splits of dataset give the input path accordingly)</i></b>
--- Scripts/parse_vibe_data.py
+++ Scripts/parse_vibe_data.py
@@ -6,20 +6,18 @@ import sys
 import h5py
 
 
-vibe_dir = '' # folder containing the json files.
+vibe_dir = sys.argv[1] # folder containing the json files.
 reqd_joints = [38, 43, 44, 45, 46, 47, 48, 40, 37, 33,32,31, 34,35,36, 41, 39, 27,26,25, 28,29,30, 22, 19]
 idx = 0
 files_order = []
 joint_data_final = []
 labels_final = []
-for num, class_name in enumerate(os.listdir(vibe_dir)):
-    for file_name in os.listdir(os.path.join(vibe_dir, class_name)): # add path to the json files here
-    #     print(j)
-    #     file_name = os.path.join(vibe_dir,j)
+for num, class_name in sorted(enumerate(os.listdir(vibe_dir))):
+    for file_name in os.listdir(os.path.join(vibe_dir, class_name)):
         print(file_name)
         with open (file_name, 'r') as f:
             data = json.load(f)
-        files_order.append(file_name)
+        # files_order.append(file_name)
         f.close()
         person_list = list(data.keys())
         if (person_list == []):
@@ -52,7 +50,7 @@ for num, class_name in enumerate(os.listdir(vibe_dir)):
                 if len(frames[frames>mf]) != 0 and len(frames[frames<mf]) != 0:
                     e = min(frames[frames>mf])
                     s = max(frames[frames<mf])
-                    final_data[mf, 0:75] = final_data[s, 0:75]*((mf - s)/(e-s)) + final_data[e, 0:75]*((e - mf)/(e-s)) 
+                    final_data[mf, 0:75] = final_data[s, 0:75]*((mf - s)/(e-s)) + final_data[e, 0:75]*((e - mf)/(e-s))
 
             frames = np.array(data[person_list[person_id[1]]]['frame_ids'])
             joint_data_2 = np.array(data[person_list[person_id[1]]]['joints3d'])[:,reqd_joints,:]
@@ -63,7 +61,7 @@ for num, class_name in enumerate(os.listdir(vibe_dir)):
                 if len(frames[frames>mf]) != 0 and len(frames[frames<mf]) != 0:
                     e = min(frames[frames>mf])
                     s = max(frames[frames<mf])
-                    final_data[mf, 75:] = final_data[s, 75:]*((mf - s)/(e-s)) + final_data[e, 75:]*((e - mf)/(e-s)) 
+                    final_data[mf, 75:] = final_data[s, 75:]*((mf - s)/(e-s)) + final_data[e, 75:]*((e - mf)/(e-s))
 
             # print (final_data.shape)
         joint_data_final.append(final_data)
@@ -75,17 +73,16 @@ print (joint_data_final.shape)
 print (labels_final.shape)
 
 
-# file_n = h5py.File('dataset.h5','w')
-# file_n.create_dataset('x',data = joint_data_final)
-# file_n.create_dataset('y',data = labels_final)
-# file_n.close()
+out_path = sys.argv[2]
+file_n = h5py.File(os.path.append(out_path, 'vacnn_data.h5'),'w')
+file_n.create_dataset('x', data = joint_data_final)
+file_n.create_dataset('y', data = labels_final)
+file_n.close()
 # 147 GB 85 GB
 
-os.mkdir('/ssd_scratch/cvit/pranay.gupta/ntu_splits_msg3d/'+str(ss)+'_'+st+'/') # path where weights need to be saved
 train_x = np.array(joint_data_final).reshape(-1,300,2,25,3).transpose((0,4,1,3,2))
 train_x = train_x[:,:,:,:,:]
-np.save('path for the new dataset', train_x)
+np.save(os.path.append(out_path, 'gcn_train_data.npy'), train_x)
 
 train_y = np.argmax(labels_final, -1)
-np.save('path for the new dataset', train_y)
-
+np.save(os.path.append(out_path, 'gcn_train_label.npy'), train_y)
